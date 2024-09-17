112d1d8a0c1dd2909f84f3e3251f86c3b5fcfd61
SalxIeva <ieva.saldukaite@gmail.com>
2024-09-17 20:40:21 +0100

    js skeleton and pseudo code added

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,22 @@
-console.log("Hello World!")
+// console.log("Hello World!")
+
+
+// All of your book objects are going to be stored in an array
+const myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+// add a function that can take user’s input and store the new book objects into an array.
+function addBookToLibrary() {
+  // do stuff here
+}
+
+
+// Write a function that loops through the array and displays each book on the page. 
+// Add a “NEW BOOK” button that brings up a form allowing users to input the details for the new book: author, title, number of pages, whether it’s been read and anything else you might want. 
+// Add a button on each book’s display to remove the book from the library.
+
+// Add a button on each book’s display to change its read status. 
+// Create the function that toggles a book’s read status on your Book prototype instance.
