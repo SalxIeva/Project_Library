5283a28ef99a73dd0cba33700ff7cf0995955304
SalxIeva <ieva.saldukaite@gmail.com>
2024-09-19 20:39:06 +0100

    Book constructor created

--- assets/script.js
+++ assets/script.js
@@ -46,12 +46,18 @@ readBtn.addEventListener('click', () => {
         readBtn.textContent = 'Read';
     }
     
-})
+});
+
 // All of your book objects are going to be stored in an array
 const myLibrary = [];
 
-function Book() {
+function Book(title, author, type, pages, read) {
   // the constructor...
+  this.title = title;
+  this.author = author;
+  this.type = type;
+  this.pages = pages;
+  this.read = read;
 }
 
 // add a function that can take user’s input and store the new book objects into an array.
