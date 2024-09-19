ab59eadf09e4018ec4bab05dbe4376dffc1c1b4f
SalxIeva <ieva.saldukaite@gmail.com>
2024-09-19 21:30:05 +0100

    displayBooks() called inside addBookToLibrary()

--- assets/script.js
+++ assets/script.js
@@ -23,21 +23,18 @@ const tableType = document.getElementById('table_type');
 const tablePages = document.getElementById('table_pages');
 const tableRead = document.getElementById('table_read');
 
-addBookBtn.addEventListener('click', () => {
-    dialog.showModal();
-});
 
 confirmBtn.addEventListener('click', () => {
     event.preventDefault();
-
+    
     addBookToLibrary();
-
+    
     tableTitle.textContent = title.value;
     tableAuthor.textContent = author.value;
     tableType.textContent = type.value;
     tablePages.textContent = pages.value;
     tableRead.textContent = read.value === 'read' ? 'Read' : 'Unread';
-
+    
     dialog.close();
 });
 
@@ -62,12 +59,12 @@ readBtn.addEventListener('click', () => {
 const myLibrary = [];
 
 function Book(title, author, type, pages, read) {
-  // the constructor...
-  this.title = title;
-  this.author = author;
-  this.type = type;
-  this.pages = pages;
-  this.read = read;
+    // the constructor...
+    this.title = title;
+    this.author = author;
+    this.type = type;
+    this.pages = pages;
+    this.read = read;
 }
 
 // let newBook = new Book('blye', 'ghfh', 'hjfhd', '565', 'read');
@@ -75,16 +72,18 @@ function Book(title, author, type, pages, read) {
 
 // add a function that can take user’s input and store the new book objects into an array.
 function addBookToLibrary() {
-  // do stuff here
+    // do stuff here
     const titleVal = title.value;
     const authorVal = author.value;
     const typeVal = type.value;
     const pagesVal = pages.value;
     const readVal = read.value;
-
+    
     const newBook = new Book(titleVal, authorVal, typeVal, pagesVal, readVal);
-
+    
     myLibrary.push(newBook);
+    
+    displayBooks();
     console.log(myLibrary);
 }
 
@@ -93,37 +92,42 @@ function addBookToLibrary() {
 
 function displayBooks() {
     output.innerHTML = '';
-
+    
     myLibrary.forEach((book, index) =>{
         const newRow = `
         <tr>
         <th colspan="2">${book.title}</th>
-      </tr>
-      <tr>
+        </tr>
+        <tr>
         <td>Author</td>
         <td>${book.author}</td>
-      </tr>
-      <tr>
+        </tr>
+        <tr>
         <td>Type</td>
         <td>${book.type}</td>
-      </tr>
-      <tr>
+        </tr>
+        <tr>
         <td>Number of pages</td>
         <td>${book.pages}</td>
-      </tr>
-      <tr>
+        </tr>
+        <tr>
         <td>The book has been read</td>
         <td><button class="readBtn" data-index="${index}">${book.read}</button></td>
-      </tr>
-      <tr>
+        </tr>
+        <tr>
         <td colspan="2"><button class="removeBtn" data-index="${index}">Remove</button></td>
-      </tr>`;
-
-      output.innerHTML += newRow;
+        </tr>`;
+        
+        output.appendChild(newRow);
     })
 }
 
 // Add a “NEW BOOK” button that brings up a form allowing users to input the details for the new book: author, title, number of pages, whether it’s been read and anything else you might want. 
+
+addBookBtn.addEventListener('click', () => {
+    dialog.showModal();
+});
+
 // Add a button on each book’s display to remove the book from the library.
 
 // Add a button on each book’s display to change its read status. 
